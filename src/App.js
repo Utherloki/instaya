32997800a71bd858e51517b5dd82935bf2169bbf
Utherloki <jose.jaramer09@gmail.com>
2022-11-20 15:49:57 -0500

    np
    	deleted:    public/favicon.ico
    	deleted:    public/logo192.png
    	deleted:    public/logo512.png
    	deleted:    public/manifest.json
    	deleted:    public/robots.txt
    	deleted:    src/App.css
    	modified:   src/App.js
    	deleted:    src/App.test.js
    	modified:   src/index.js
    	deleted:    src/logo.svg
    	deleted:    src/reportWebVitals.js
    	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
 
-function App() {
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className="px-4 py-5 my-5 text-center">
+            <img class="d-block mx-auto mb-4" alt="" src="https://img.icons8.com/color/2x/bootstrap.png"></img>
+            <h1 class="display-5 fw-bold">Bootstrap en React!</h1>
+            <div class="col-lg-6 mx-auto">
+              <p class="lead mb-4">
+                Diseña y personaliza rápidamente sitios adaptables a
+                dispositivos móviles con Bootstrap. el kit de herramientas de
+                código abierto front-end más popular del mundo
+              </p>
+              <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
+                  Botón Primario
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-outline-secundary btn-lg px-4 gap-3"
+                >
+                  Botón Secundario
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
